0439f1cb2db0755f6204e68d72945f8000894785
persilee <i@lishaoy.net>
2018-11-24 00:53:34 +0800

    🚲 add resouce route

--- start/routes.js
+++ start/routes.js
@@ -1,33 +1,11 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
 Route.on('/').render('welcome')
 
-Route.get('/posts', 'PostController.index')
+Route.resource('posts', 'PostController')
+  .except(['index'])
+  // .only(['index', 'show', 'store'])
+  // .apiOnly()
 
-Route.post('/posts', 'PostController.store')
-
-Route.get('/posts/create', 'PostController.create')
-
-Route.get('/posts/:id', 'PostController.show')
-
-Route.patch('/posts/:id', 'PostController.update')
-
-Route.delete('/posts/:id', 'PostController.destroy')
-
-Route.get('/posts/:id/edit', 'PostController.edit')
