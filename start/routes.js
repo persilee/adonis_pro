b3b1bc9430a7bdfc406ae7777534746cf8181c76
persilee <i@lishaoy.net>
2018-11-24 01:26:54 +0800

    🗼 other route

--- start/routes.js
+++ start/routes.js
@@ -4,8 +4,24 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 
-Route.resource('posts', 'PostController')
-  .except(['index'])
-  // .only(['index', 'show', 'store'])
-  // .apiOnly()
+Route.resource('posts', 'PostController').except(['index'])
+// .only(['index', 'show', 'store'])
+// .apiOnly()
 
+Route.get('/list-of-users', () => 'List of users.').as('users.index')
+
+Route.get('/users', ({ request }) => {
+  switch (request.format()) {
+    case 'json':
+      return [{ name: 'lishaoy' }, { name: 'miaomiao' }]
+    default:
+      return `lishaoy`
+  }
+}).formats(['json', 'html'], true)
+
+Route.group(() => {
+  Route.get('users', () => 'Manage users')
+  Route.get('posts', () => 'Manage posts')
+}).prefix('admin')
+
+Route.any('*', ({ view }) => view.render('welcome'))
