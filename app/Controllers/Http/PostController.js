c8eee384d3d6fe5f599fae18f5c92a45bf50a81a
persilee <i@lishaoy.net>
2018-11-28 20:39:10 +0800

    ⏰ add edit post of users tags

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -14,7 +14,7 @@ const { validateAll } = use('Validator')
  * Resourceful controller for interacting with posts
  */
 class PostController {
-  /**
+	/**
    * Show a list of all posts.
    * GET posts
    *
@@ -23,18 +23,18 @@ class PostController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-    const posts = await Post.query()
-      .with('user', (builder) => {
-        builder.select('id', 'username')
-      })
-      .with('user.profile')
-      .fetch()
-    // return posts
-    return view.render('post.index', { posts: posts.toJSON() })
-  }
+	async index ({ request, response, view }) {
+		const posts = await Post.query()
+			.with('user', (builder) => {
+				builder.select('id', 'username')
+			})
+			.with('user.profile')
+			.fetch()
+		// return posts
+		return view.render('post.index', { posts: posts.toJSON() })
+	}
 
-  /**
+	/**
    * Render a form to be used for creating a new post.
    * GET posts/create
    *
@@ -43,14 +43,17 @@ class PostController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async create ({ request, response, view }) {
-    const users = await User.all()
-    const tags = await Tag.all()
+	async create ({ request, response, view }) {
+		const users = await User.all()
+		const tags = await Tag.all()
 
-    return view.render('post.create', { users: users.toJSON(), tags: tags.toJSON() })
-  }
+		return view.render('post.create', {
+			users: users.toJSON(),
+			tags: tags.toJSON()
+		})
+	}
 
-  /**
+	/**
    * Create/save a new post.
    * POST posts
    *
@@ -58,36 +61,34 @@ class PostController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response, session }) {
-    const rules = {
-      title: 'required',
-      content: 'required'
-    }
+	async store ({ request, response, session }) {
+		const rules = {
+			title: 'required',
+			content: 'required'
+		}
 
-    const validation = await validateAll(request.all(), rules)
+		const validation = await validateAll(request.all(), rules)
 
-    if (validation.fails()) {
-      session.withErrors(validation.messages()).flashAll()
+		if (validation.fails()) {
+			session.withErrors(validation.messages()).flashAll()
 
-      return response.redirect('back')
-    }
+			return response.redirect('back')
+		}
 
-    const newPost = request.only(['title', 'content'])
-    const tags = request.input('tags')
-    // const postId = await Database.insert(newPost).into('posts')
-    // console.log(postId)
-    // const post = await Post.create(newPost)
+		const newPost = request.only([ 'title', 'content' ])
+		const tags = request.input('tags')
+		// const postId = await Database.insert(newPost).into('posts')
+		// console.log(postId)
+		// const post = await Post.create(newPost)
 
-    const user = await User.find(request.input('user_id'))
-    const post = await user.posts()
-      .create(newPost)
-    await post.tags()
-      .attach(tags)
+		const user = await User.find(request.input('user_id'))
+		const post = await user.posts().create(newPost)
+		await post.tags().attach(tags)
 
-    return response.redirect(`posts/${post.id}`)
-  }
+		return response.redirect(`posts/${post.id}`)
+	}
 
-  /**
+	/**
    * Display a single post.
    * GET posts/:id
    *
@@ -96,20 +97,18 @@ class PostController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-    // const post = await Database.from('posts')
-    //   .where('id', params.id)
-    //   .first()
+	async show ({ params, request, response, view }) {
+		// const post = await Database.from('posts')
+		//   .where('id', params.id)
+		//   .first()
 
-    const post = await Post.findOrFail(params.id)
-    const tags = await post.tags()
-      .select('id', 'title')
-      .fetch()
+		const post = await Post.findOrFail(params.id)
+		const tags = await post.tags().select('id', 'title').fetch()
 
-    return view.render('post.show', { post, tags: tags.toJSON() })
-  }
+		return view.render('post.show', { post, tags: tags.toJSON() })
+	}
 
-  /**
+	/**
    * Render a form to update an existing post.
    * GET posts/:id/edit
    *
@@ -118,16 +117,43 @@ class PostController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async edit ({ params, request, response, view }) {
-    // const post = await Database.from('posts')
-    //   .where('id', params.id)
-    //   .first()
+	async edit ({ params, request, response, view }) {
+		// const post = await Database.from('posts')
+		//   .where('id', params.id)
+		//   .first()
 
-    const post = await Post.findOrFail(params.id)
-    return view.render('post.edit', { post: post.toJSON() })
-  }
+		const _post = await Post.findOrFail(params.id)
+    const users = await User.all()
+    const _tags = await Tag.all()
+    const tags = _tags.toJSON()
+    await _post.load('tags')
+    const post = _post.toJSON()
+    const postTagIds = post.tags.map(tag => tag.id)
 
-  /**
+    const tagItems = tags.map((tag) => {
+      if (postTagIds.includes(tag.id)) {
+        tag.check = true
+      }
+
+      return tag
+    })
+
+		const userItems = users.toJSON().map((user) => {
+			if (user.id === post.user_id) {
+				user.check = true
+			}
+
+			return user
+		})
+
+		return view.render('post.edit', {
+			post: post,
+      users: userItems,
+      tags: tagItems
+		})
+	}
+
+	/**
    * Update post details.
    * PUT or PATCH posts/:id
    *
@@ -135,18 +161,18 @@ class PostController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
-    const updatedPost = request.only(['title', 'content'])
-    // await Database.table('posts')
-    //   .where('id', params.id)
-    //   .update(updatedPost)
+	async update ({ params, request, response }) {
+		const updatedPost = request.only([ 'title', 'content' ])
+		// await Database.table('posts')
+		//   .where('id', params.id)
+		//   .update(updatedPost)
 
-    const post = await Post.findOrFail(params.id)
-    post.merge(updatedPost)
-    post.save()
-  }
+		const post = await Post.findOrFail(params.id)
+		post.merge(updatedPost)
+		post.save()
+	}
 
-  /**
+	/**
    * Delete a post with id.
    * DELETE posts/:id
    *
@@ -154,20 +180,20 @@ class PostController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
-    // await Database.table('posts')
-    //   .where('id', params.id)
-    //   .delete()
+	async destroy ({ params, request, response }) {
+		// await Database.table('posts')
+		//   .where('id', params.id)
+		//   .delete()
 
-    const post = await Post.find(params.id)
-    try {
-      await post.tags().detach()
-      post.delete()
-    } catch (error) {
-      console.log(error)
-    }
-    return 'success'
-  }
+		const post = await Post.find(params.id)
+		try {
+			await post.tags().detach()
+			post.delete()
+		} catch (error) {
+			console.log(error)
+		}
+		return 'success'
+	}
 }
 
 module.exports = PostController
