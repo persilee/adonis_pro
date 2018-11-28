4b23ad095cf29144dcb28011ca74734280900b74
persilee <i@lishaoy.net>
2018-11-29 03:05:21 +0800

    🚂 add file list adn modify setting.json

--- app/Controllers/Http/FileController.js
+++ app/Controllers/Http/FileController.js
@@ -5,12 +5,13 @@
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
 const Helpers = use('Helpers')
+const File = use('App/Models/File')
 
 /**
  * Resourceful controller for interacting with files
  */
 class FileController {
-  /**
+	/**
    * Show a list of all files.
    * GET files
    *
@@ -19,10 +20,14 @@ class FileController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+	async index ({ request, response, view }) {
+		const _files = await File.all()
+		const files = _files.toJSON()
 
-  /**
+		return view.render('file.index', { files })
+	}
+
+	/**
    * Render a form to be used for creating a new file.
    * GET files/create
    *
@@ -31,11 +36,11 @@ class FileController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async create ({ request, response, view }) {
-    return view.render('file.create')
-  }
+	async create ({ request, response, view }) {
+		return view.render('file.create')
+	}
 
-  /**
+	/**
    * Create/save a new file.
    * POST files
    *
@@ -43,38 +48,46 @@ class FileController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response, session }) {
-    const file = request.file('file', {
-      types: ['image', 'video'],
-      size: '100mb'
-    })
+	async store ({ request, response, session }) {
+		const file = request.file('file', {
+			types : [ 'image', 'video' ],
+			size  : '100mb'
+		})
 
-    const fileName = `${new Date().getTime() }.${ file.subtype }`
+		const fileName = `${new Date().getTime()}.${file.subtype}`
 
-    await file.move(Helpers.publicPath('uploads'), {
-      name: fileName
-    })
+		await file.move(Helpers.publicPath('uploads'), {
+			name : fileName
+		})
 
-    if (!file.moved()) {
-      const error = file.error()
+		if (!file.moved()) {
+			const error = file.error()
 
-      session.flash({
-        type: 'warning',
-        message: `<small>${ error.clientName }</small>: ${ error.message }`
-      })
+			session.flash({
+				type    : 'warning',
+				message : `<small>${error.clientName}</small>: ${error.message}`
+			})
 
-      return response.redirect('back')
-    }
+			return response.redirect('back')
+		}
 
-    session.flash({
-      type: 'success',
-      message: `<small>${error.clientName}</small>: Successfully uploaded.`
-    })
+		await File.create({
+			client_name : file.clientName,
+			file_name   : fileName,
+			type        : file.type,
+			subtype     : file.subtype,
+			size        : file.size
+		})
 
-    return response.redirect('back')
-  }
+		session.flash({
+			type    : 'success',
+			message : `<small>${file.clientName}</small>: Successfully uploaded.`
+		})
 
-  /**
+		return response.redirect('back')
+	}
+
+	/**
    * Display a single file.
    * GET files/:id
    *
@@ -83,10 +96,9 @@ class FileController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+	async show ({ params, request, response, view }) {}
 
-  /**
+	/**
    * Render a form to update an existing file.
    * GET files/:id/edit
    *
@@ -95,10 +107,9 @@ class FileController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async edit ({ params, request, response, view }) {
-  }
+	async edit ({ params, request, response, view }) {}
 
-  /**
+	/**
    * Update file details.
    * PUT or PATCH files/:id
    *
@@ -106,10 +117,9 @@ class FileController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
-  }
+	async update ({ params, request, response }) {}
 
-  /**
+	/**
    * Delete a file with id.
    * DELETE files/:id
    *
@@ -117,8 +127,7 @@ class FileController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
-  }
+	async destroy ({ params, request, response }) {}
 }
 
 module.exports = FileController
