1904bda0c5db737478ed09173ecaf478319583bf
persilee <i@lishaoy.net>
2018-11-29 19:33:46 +0800

    🤺  pagination of method is done

---
+++ start/hooks.js
@@ -0,0 +1,55 @@
+const { hooks } = require('@adonisjs/ignitor')
+const { range } = require('lodash')
+
+hooks.after.providersBooted(() => {
+  const View = use('View')
+
+  View.global('pageItems', (lastPage, page) => {
+    const allPageItems = range(1, lastPage + 1)
+    const pageItemRange = 2
+    const pageItemAfter = allPageItems.slice(page, page + pageItemRange)
+    const pageItemBefore = allPageItems.slice(page - lastPage - pageItemRange - 1, page - 1)
+    let pageItems = [
+      ...pageItemBefore,
+      page,
+      ...pageItemAfter
+    ]
+
+    let firstItem = [1]
+    let lastItem = [lastPage]
+
+    if (pageItemRange + 2 < page) {
+      firstItem = [
+        ...firstItem,
+        '...'
+      ]
+    }
+
+    if (lastPage - page -1 > pageItemRange) {
+      lastItem = [
+        '...',
+        ...lastItem
+      ]
+    }
+
+    if (pageItemRange + 1 < page) {
+      pageItems = [
+        ...firstItem,
+        ...pageItems
+      ]
+    }
+
+    if (lastPage - page > pageItemRange) {
+      pageItems = [
+        ...pageItems,
+        ...lastItem
+      ]
+    }
+
+    return pageItems
+  })
+
+  View.global('parseInt', (value) => {
+    return parseInt(value)
+  })
+})
