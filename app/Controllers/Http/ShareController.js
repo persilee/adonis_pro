9b8d737da2c59d07ae482112f18e485047789dbc
persilee <i@lishaoy.net>
2018-11-30 15:46:24 +0800

    🤽‍♀️ add mail style

--- app/Controllers/Http/ShareController.js
+++ app/Controllers/Http/ShareController.js
@@ -2,34 +2,55 @@
 
 const Post = use('App/Models/Post')
 const Mail = use('Mail')
-
+/**
+ * 邮件分享功能
+ *
+ * @class ShareController
+ */
 class ShareController {
-  async email ({ auth, params, request, response, session }) {
-    const user = await auth.getUser()
+	/**
+   * 点击邮件 小图标可发送邮件到用户的邮箱
+   *
+   * @param {*} { auth, params, request, response, session }
+   * @returns
+   * @memberof ShareController
+   */
+	async email ({ auth, params, request, response, session }) {
+		const user = await auth.getUser()
 
-    switch (params.type) {
-      case 'post':
-        const post = await Post.find(params.id)
-        const author = await post.user().fetch()
+		switch (params.type) {
+			case 'post':
+				const post = await Post.find(params.id)
+				const author = await post.user().fetch()
 
-        await Mail.raw(`<div><h1>${ post.title }</h1>${ post.content }</div>`, (message) => {
-          message.to(user.email)
-            .from('dev-demo@lishaoy.net')
-            .subject(`《${ post.title }》 - ${ author.username }`)
-        })
-        break;
+				// await Mail.raw(`<div><h1>${ post.title }</h1>${ post.content }</div>`, (message) => {
+				//   message.to(user.email)
+				//     .from('dev-demo@lishaoy.net')
+				//     .subject(`《${ post.title }》 - ${ author.username }`)
+				// })
+				await Mail.send(
+					'email.share_post',
+					{ host: request.header('host'), post: post.toJSON(), user: user.toJSON() },
+					(message) => {
+						message
+							.to(user.email)
+							.from('dev-demo@lishaoy.net')
+							.subject(`《${post.title}》 - ${author.username}`)
+					}
+				)
+				break
 
-      default:
-        break;
-    }
+			default:
+				break
+		}
 
-    session.flash({
-      type: 'success',
-      message: 'Mail has been sent, check your inbox.'
-    })
+		session.flash({
+			type    : 'success',
+			message : 'Mail has been sent, check your inbox.'
+		})
 
-    return response.redirect('back')
-  }
+		return response.redirect('back')
+	}
 }
 
 module.exports = ShareController
