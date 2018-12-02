f03d4969553f9cdf1ba0472bc349bd95cf80d7ce
persilee <i@lishaoy.net>
2018-12-02 20:12:20 +0800

    🌶 done

--- app/Controllers/Ws/WsDemoController.js
+++ app/Controllers/Ws/WsDemoController.js
@@ -1,9 +1,21 @@
 'use strict'
 
 class WsDemoController {
-  constructor ({ socket, request }) {
+  constructor ({ socket, request, auth }) {
     this.socket = socket
     this.request = request
+    this.user = auth.user || { username: 'Anonymous' }
+  }
+
+  onMessage (message) {
+    console.log(message)
+    const { username } = this.user
+    const { content } = message
+
+    this.socket.broadcastToAll('message', {
+      username,
+      content
+    })
   }
 }
 
