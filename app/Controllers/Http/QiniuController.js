96e1b3587b4106d3f27cc7fb6016faf23f76d1d9
persilee <i@lishaoy.net>
2018-12-21 15:19:06 +0800

    🍧 modify config file

--- app/Controllers/Http/QiniuController.js
+++ app/Controllers/Http/QiniuController.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const Env = use('Env')
+
 class QiniuController {
 	async imageUpload ({ params }) {
 		const qiniu = require('qiniu')
 
-    const accessKey = 'nulLMpS6oXHaG7MBV41RAQ--VCgepFK_-IfTKnBM'
-    const secretKey = 'RyOJ8RN8NDWokXVGHF8WFiM2lfesbaeh8-m0-QbS'
+    const accessKey = Env.get('ACCESS_KEY')
+    const secretKey = Env.get('SECRET_KEY')
     const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
     const options = {
       scope: 'myblog',
